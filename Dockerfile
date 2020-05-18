4b8042f5dbfa707fb0853c76b552a42e2f9efded
mejia414 <mejia414@hotmail.com>
2020-05-18 18:46:37 -0500

    final com

---
+++ Dockerfile
@@ -0,0 +1,7 @@
+FROM python:3.6-alpine
+
+ADD . /code
+WORKDIR /code
+RUN pip install -r requirements.txt
+EXPOSE 4000
+CMD ["python", "app.py"]
--- app.py
+++ app.py
@@ -1,21 +1,25 @@
 from flask import Flask
 from flask import jsonify
 from flask import render_template
+from flask import Request
+from flask import Response
+from flask import redirect
+
 
 app = Flask(__name__)
 
 
 @app.route('/', methods=['GET'])
 def index():
-    response =  render_template('index.html', msg='hello world')
+    response = render_template('index.html', msg='hello world')
     return response
 
 
 @app.route('/api', methods=['GET'])
 def api():
-    response = jsonify({'response':'hello world'})
+    response = jsonify({'response': 'hello world'})
     return response
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=4000, debug=True)
